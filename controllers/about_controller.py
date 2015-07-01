4471fc80502b4001acafbbabf7aed287fe661166
Omar H. Ayad <omarayad1@gmail.com>
2015-07-01 23:13:37 +0200

    fixed models load

--- controllers/about_controller.py
+++ controllers/about_controller.py
@@ -1,8 +1,8 @@
 from flask.views import MethodView
 from flask import render_template
-import models
+from models import about
 
 class AboutController(MethodView):
 	def get(self):
-		data_about = models.about.About.objects
+		data_about = about.About.objects
 		return render_template('about.html', data=data_about)
--- controllers/projects_controller.py
+++ controllers/projects_controller.py
@@ -1,15 +1,15 @@
 from flask.views import MethodView
 from flask import render_template
 from helpers import markdown_converter
-import models
+from models import projects
 
 class ProjectsController(MethodView):
 	def get(self,id):
 		if id is None:
-			data_projects = models.projects.Projects.objects
+			data_projects = projects.Projects.objects
 			return render_template('projects.html', data=data_projects)
 		else:
-			data = models.projects.Projects.objects(pk=id).first()
+			data = projects.Projects.objects(pk=id).first()
 			md = []
 			for el in data.markdown:
 				html = markdown_converter.convert(el.markdown)
